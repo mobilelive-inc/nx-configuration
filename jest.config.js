a722557d30bb3f52ad095ffbc5b781bdafdaa22b
ahmedbilal1994 <ahmedbilal@mobilelive.ca>
2021-06-25 18:33:35 +0500

    resolved naming conventions

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,9 @@
 module.exports = {
-  projects: ['<rootDir>/apps/react-demos', '<rootDir>/apps/angular-demos']
+  projects: [
+    '<rootDir>/apps/react-demos',
+    '<rootDir>/apps/angular-demos',
+    '<rootDir>/libs/react',
+    '<rootDir>/libs/angular',
+    '<rootDir>/libs/static'
+  ]
 };
---
+++ libs/react/src/lib/react.tsx
@@ -0,0 +1,14 @@
+import './react.module.scss';
+
+/* eslint-disable-next-line */
+export interface ReactProps {}
+
+export function React(props: ReactProps) {
+  return (
+    <div>
+      <h1>Welcome to react!</h1>
+    </div>
+  );
+}
+
+export default React;
