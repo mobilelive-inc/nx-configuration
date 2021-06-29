7ef8e9393da423115a3e20e967b9d81ebd30c319
Muhammad Ahmad <ahmad.muhammad@mobilelive.ca>
2021-06-29 14:38:36 +0500

    Button and Card Component imported for testing

--- libs/angular/src/lib/angular.module.ts
+++ libs/angular/src/lib/angular.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ButtonComponent } from './button/button.component';
+import { CardComponent } from './card/card.component';
+import { Template, Header, Footer } from  './shared/template.directive'
 
 @NgModule({
-  imports: [CommonModule]
+  imports: [CommonModule],
+  declarations: [
+    ButtonComponent,
+    CardComponent,
+    Template,
+    Header, Footer
+  ],
+  exports: [ ButtonComponent, CardComponent, Template, Header, Footer]
 })
 export class AngularModule {}
