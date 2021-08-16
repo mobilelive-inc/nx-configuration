34d44a394f360c49ee5c56d390dce8bf8a19fcb1
Hamza Rafique <hamza.rafique@mobilelive.ca>
2021-08-16 16:36:22 +0500

    button story issue of theme resolved

--- libs/react/src/index.ts
+++ libs/react/src/index.ts
@@ -1,1 +1,3 @@
 export * from './lib/react';
+export * from './lib/buttons/buttons';
+export * from './lib/button/button'
---
+++ libs/react/src/lib/buttons/buttons.spec.tsx
@@ -0,0 +1,9 @@
+import { render } from '@testing-library/react';
+import Buttons from './buttons';
+
+describe('Buttons', () => {
+  it('should render successfully', () => {
+    const { baseElement } = render(< Buttons />);
+    expect(baseElement).toBeTruthy();
+  });
+});
