3f6cfdb9e435b87623c88d1cdb58523be144caf4
Hamza Rafique <hamza.rafique@mobilelive.ca>
2021-08-20 21:39:42 +0500

    advanced-stepper is added

--- libs/react/src/index.ts
+++ libs/react/src/index.ts
@@ -1,3 +1,5 @@
+
+export * from './lib/advanced-stepper/advanced-stepper';
 export * from './lib/react';
 export * from './lib/buttons/buttons';
 export * from './lib/button/button'
---
+++ libs/react/src/lib/advanced-stepper/advanced-stepper.spec.tsx
@@ -0,0 +1,10 @@
+import { render } from '@testing-library/react';
+
+import AdvancedStepper from './advanced-stepper';
+
+describe('AdvancedStepper', () => {
+  it('should render successfully', () => {
+    const { baseElement } = render(< AdvancedStepper />);
+    expect(baseElement).toBeTruthy();
+  });
+});
